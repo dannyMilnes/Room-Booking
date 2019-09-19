cb76e56903a6327c4dc0122cafa9744da0bb6841
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2019-09-19 16:33:44 +0100

    first routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./components/home/home.component";
+
+import { RouterModule, Routes } from "@angular/router";
 import { BookingsComponent } from "./components/bookings/bookings.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   {
     path: "bookings",
     component: BookingsComponent
-  },
-  { path: "**", component: PageNotFoundComponent }
+  }
+  // { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
