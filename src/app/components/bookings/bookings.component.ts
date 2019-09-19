83b0b6e11695e06224e8cb8e78f2883c27b6142e
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2019-09-19 12:22:21 +0100

    initial styling for bookings page

--- src/app/components/bookings/bookings.component.ts
+++ src/app/components/bookings/bookings.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.scss']
+  selector: "app-bookings",
+  templateUrl: "./bookings.component.html",
+  styleUrls: ["./bookings.component.scss"]
 })
 export class BookingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
